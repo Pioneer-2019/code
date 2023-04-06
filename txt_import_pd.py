b6becd0efde5afbf67dcd360f9a15f787ba3f433
Hua <2480975163@qq.com>
2023-04-07 00:55:16 +0800

    第二次提交，完成TXT文件导入功能

---
+++ txt_import_pd.py
@@ -0,0 +1,84 @@
+#导入数据分析pandas库
+import pandas as pd #
+#导入查找文件目录和文件名的模块
+import glob 
+#导入系统os库
+import os
+
+'''
+#方法一：获取指定目录下符合条件文件的路径
+#TXT文件路径字符串-*通配符：所有TXT文件
+path = r"D:\Study\CPK_Automate_Tool\test\*.txt"
+#将符合路径条件的TXT文件路径生成列表文件
+all_file_path=glob.glob(path)
+#print(all_file_path)
+'''
+
+#方法二：获取各子目录下文件路径
+path=r'D:\Study\CPK_Automate_Tool\test'  #存放数据的根目录
+# 获取所有文件的完整路径名
+all_files_path=[]
+for root,dirs,files in os.walk(path,topdown=False):   #os.walk()方法，是用于遍历文件目录，并返回目录名称的方法；root 所指的是当前正在遍历的这个文件夹的本身的地址；files 同样是 list , 内容是该文件夹中所有的文件(不包括子目录)
+    
+    #print(root)
+    #print(dirs)
+    #print(files)
+    if len(files)>0:  #空文件夹没有文件的无需读取
+        each_foder_files=[os.path.join(root,x) for x in files] #通过目录root+文件名file,合成每个文件的路径名
+        #print(each_foder_files)
+        all_files_path.extend(each_foder_files)
+
+#print(all_files_path)
+
+
+
+
+
+
+
+#创建空dataframe
+#df_temp= pd.DataFrame(columns=['序号'])
+#创建一个计数变量以读取的文件数量
+#path_num=1
+
+#读取第一个文件
+df_temp=pd.read_csv(all_files_path[0],sep="=",header=3,usecols=[1],dtype={0:int})  #,nrows=40
+
+#循环读取TXT文件到dataframe数据结构中
+for path_each in all_files_path:
+    #每个文件读取为1个dataframe
+    df_each=pd.read_csv(path_each,sep="=",header=3,usecols=[1],dtype={0:int}) #,nrows=40
+    #print(df_each)
+    #不断合并dataframe
+    df_temp=pd.concat([df_temp,df_each],axis=1,join='outer',ignore_index=True)
+    #print(df_temp)
+
+print(df_temp)
+
+
+
+
+
+
+'''
+f_path1= r"D:\Study\CPK_Automate_Tool\test\FQ29PB006M-20230131_023809_NSFT_PASS_1.txt"
+f_path2= r"D:\Study\CPK_Automate_Tool\test\FQ29PB006Y-20230131_121817_NSFT_PASS.txt"
+f_path3= r"D:\Study\CPK_Automate_Tool\test\FQ29PB006W-20230131_061339_NSFT_PASS.txt"
+
+df1=pd.read_csv(f_path1,sep="=",header=3,usecols=[1],dtype={0:int},nrows=40) 
+df2=pd.read_csv(f_path2,sep="=",header=3,usecols=[1],dtype={0:int},nrows=40) 
+df3=pd.read_csv(f_path3,sep="=",header=3,usecols=[1],dtype={0:int},nrows=40) 
+
+#print(df)
+
+df0=pd.concat([df1,df2,df3],axis=1)  #按列进行拼接
+print(df0)
+'''
+
+'''
+    if len(files)>0:
+        each_foder_files=[os.path.join(root,x) for x in files]
+        #print(each_foder_files)
+        all_files_path.extend(each_foder_files)
+        #print(all_files_path)
+'''
--- txt_inport.py
+++ txt_inport.py
@@ -1,11 +1,29 @@
-with open('D:\学习\CPK自动化工具\data\demo\FQ29PB006P-20230131_024904_NSFT_PASS.txt','r',encoding = 'utf-8') as f:
+f_path=r'D:\Study\CPK_Automate_Tool\test\FQ29PB006M-20230131_023809_NSFT_PASS_1.txt'
+with open(f_path,'r',encoding = 'utf-8') as f:
     temp = f.read() #导入文件
+    #print(temp)
 line_list = temp.splitlines() #将文件按行转化为列表
 #print(line_list)
 front=line_list.index('[TEST_DATA]') #检索开始标识[TEST_DATA]的索引值
 end=line_list.index("[TEST_RESULT]")#检索结束标识[TEST_RESULT]的索引值
 list_need=line_list[front+1:end] #截取开始标识及结束标识之间的数据
 #print(list_need)
+list_end=[]
+for i in list_need:   #遍历列表中的元素字符串
+    #print(i)
+    #print(i.split('='))
+    #print(i.split('=')[1])
+    test_value=i.split('=')[1] #使用分隔符“=”拆分字符串并获取索引为1的等号右边的测试值
+    list_end.append(test_value) #将测试值填入列表
+
+#print(list_end)
+"""
+line_list = temp.splitlines() #将文件按行转化为列表
+#print(line_list)
+front=line_list.index('[TEST_DATA]') #检索开始标识[TEST_DATA]的索引值
+end=line_list.index("[TEST_RESULT]")#检索结束标识[TEST_RESULT]的索引值
+list_need=line_list[front+1:end] #截取开始标识及结束标识之间的数据
+print(list_need)
 
 list_end=[]
 for i in list_need:   #遍历列表中的元素字符串
@@ -16,3 +34,4 @@ for i in list_need:   #遍历列表中的元素字符串
     list_end.append(test_value) #将测试值填入列表
 
 print(list_end)
+"""
