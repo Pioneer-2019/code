72c4a938f7938036d8fcb61071cecacc8f9712a2
Hua <2480975163@qq.com>
2023-04-19 23:46:13 +0800

    修改代码满足正则表达式筛选测试项

--- dataframe_re.py
+++ dataframe_re.py
@@ -1,5 +1,6 @@
+#导入数据分析pandas 库
 import pandas as pd
-
+#导入正则表达式模块
 import re
 
 #存放MES LOG TXT目标文件的路径
@@ -7,16 +8,9 @@ path=r'D:\Study\CPK_Automate_Tool\test\FQ29PB006J-20230130_203239_NSFT_PASS.txt'
 
 #读取一个MES LOG TXT文件到dataframe
 df_temp=pd.read_csv(path,sep="=") 
-print(df_temp)
 print(df_temp['[HEAD]'])
-print(df_temp[df_temp['[HEAD]'].str.contains('..FT',na=False)])  #na=False的意思就是，遇到非字符串的情况，直接忽略。你也可以写na=True，意思就是遇到非字符串的情况，计为筛选有效。
-print(df_temp[df_temp['[HEAD]'].str.contains(r'[0-9A-Z]{10}',na=False)]) 
-print(df_temp[df_temp['[HEAD]'].str.contains(r'\w{10}',na=False)]) 
-print(df_temp[df_temp['[HEAD]'].str.contains(r'\d',na=False)]) 
 
-print(df_temp[df_temp['[HEAD]'].str.match(r'[0-9A-Z]{10}',na=False)]) 
-print(df_temp[df_temp['[HEAD]'].str.match(r'[A-Z][0-9A-Z]{9}',na=False)]) 
-print(df_temp[df_temp['[HEAD]'].str.match(r'[0-9]{15}',na=False)]) 
-print(df_temp[df_temp['[HEAD]'].str.match(r'[0-9]{15}[^,]',na=False)]) 
-#print(df_temp[0].str)
-#df_temp[df_temp[0]]
+#使用正则表达式筛选出特定规律的行
+print(df_temp[df_temp['[HEAD]'].str.match(r'[A-Z][0-9A-Z]{9}',na=False)]) #筛选出SN项，10位数字及大写字母字符串，第一位为大写字母  #na=False的意思就是，遇到非字符串的情况，直接忽略。你也可以写na=True，意思就是遇到非字符串的情况，计为筛选有效。
+print(df_temp[df_temp['[HEAD]'].str.match(r'\b[0-9]{15}$',na=False)])  #筛选出IMEI项，15位数字型字符串
+print(df_temp[df_temp['[HEAD]'].str.match(r'[-]*\d{1,4}[.]?\d{0,5}$\b',na=False)])  #筛选出纯数值的测试项，模式主要有 -xx.xx;xx;xx.xx
